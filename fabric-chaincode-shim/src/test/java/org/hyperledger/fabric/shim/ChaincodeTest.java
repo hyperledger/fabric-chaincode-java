455100c1c98fcb1e340d71216c1104c9ae72b68a
Albert Lacambra Basil <albert@lacambra.tech>
2018-11-08 06:41:25 +0100

    [FAB-12426] Fix java chaincode return status.
    Java chaincode should be able to return any status it wants, not limited to SUCCESS(200) and INTERNAL_SERVER_ERROR(500)
    Direct integer codes are also supported.
    
    Change-Id: Iacbed9ab30f7c0f61301a9af45759359855c4fcb
    Signed-off-by: Albert Lacambra Basil <albert@lacambra.tech>

--- fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/shim/ChaincodeTest.java
+++ fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/shim/ChaincodeTest.java
@@ -11,7 +11,7 @@ import org.junit.rules.ExpectedException;
 
 import java.nio.charset.StandardCharsets;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ChaincodeTest {
 
@@ -26,9 +26,31 @@ public class ChaincodeTest {
         assertEquals("Incorrect payload", "no payload", resp.getStringPayload());
     }
 
+
+    @Test
+    public void testResponseWithCode() {
+        Chaincode.Response resp = new Chaincode.Response(200, "No message", "no payload".getBytes(StandardCharsets.UTF_8));
+        assertEquals("Incorrect status", Chaincode.Response.Status.SUCCESS, resp.getStatus());
+        assertEquals("Incorrect status", 200, resp.getStatusCode());
+        assertEquals("Incorrect message", "No message", resp.getMessage());
+        assertEquals("Incorrect payload", "no payload", resp.getStringPayload());
+
+        resp = new Chaincode.Response(404, "No message", "no payload".getBytes(StandardCharsets.UTF_8));
+        assertEquals("Incorrect status", 404, resp.getStatusCode());
+        assertEquals("Incorrect message", "No message", resp.getMessage());
+        assertEquals("Incorrect payload", "no payload", resp.getStringPayload());
+
+        resp = new Chaincode.Response(Chaincode.Response.Status.ERROR_THRESHOLD, "No message", "no payload".getBytes(StandardCharsets.UTF_8));
+        assertEquals("Incorrect status", Chaincode.Response.Status.ERROR_THRESHOLD, resp.getStatus());
+        assertEquals("Incorrect status", 400, resp.getStatusCode());
+        assertEquals("Incorrect message", "No message", resp.getMessage());
+        assertEquals("Incorrect payload", "no payload", resp.getStringPayload());
+    }
+
     @Test
     public void testStatus() {
         assertEquals("Wrong status", Chaincode.Response.Status.SUCCESS, Chaincode.Response.Status.forCode(200));
+        assertEquals("Wrong status", Chaincode.Response.Status.ERROR_THRESHOLD, Chaincode.Response.Status.forCode(400));
         assertEquals("Wrong status", Chaincode.Response.Status.INTERNAL_SERVER_ERROR, Chaincode.Response.Status.forCode(500));
 
         thrown.expect(IllegalArgumentException.class);
