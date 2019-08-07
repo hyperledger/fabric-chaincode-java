25ed6c183fe485f6a0e22678a72d61973713a173
heatherlp <heatherpollard0@gmail.com>
2019-08-07 16:41:52 +0100

    [FABN-1320] Fix git_tag.sh
    - so that it parses the build.gradle version
    - also specify a version and add a task to print it in the root level build.gradle
    
    Change-Id: I376bb53765cf21a715d084719eac2345ba44885d
    Signed-off-by: heatherlp <heatherpollard0@gmail.com>
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

---
+++ scripts/gittag.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Exit on first error, print all commands.
+set -e
+set -o pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+# release name
+RELEASE=release-1.4
+
+function abort {
+	echo "!! Exiting shell script"
+	echo "!!" "$1"
+	exit -1
+}
+
+# Run printVersionName task in the root directory, grab the first line and remove anything after the version number
+VERSION=$(cd ../ && ./gradlew -q printVersionName | gsed -n 1p | gsed -r "s/-.*//")
+
+echo New version string will be v${VERSION}
+
+# do the release notes for this new version exist?
+if [[ -f "${DIR}/release_notes/v${VERSION}.txt" ]]; then
+   echo "Release notes exist, hope they make sense!"
+else
+   abort "No releases notes under the file ${DIR}/release_notes/v${NEW_VERSION}.txt exist";
+fi
+
+git checkout "${RELEASE}"
+git pull
+git tag -a "v${VERSION}" `git log -n 1 --pretty=oneline | head -c7` -F release_notes/"v${VERSION}".txt
+git push origin v${VERSION} HEAD:refs/heads/${RELEASE}
