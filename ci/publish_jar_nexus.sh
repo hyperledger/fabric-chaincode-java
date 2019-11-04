5a11c6e538f5b31fb966554577a62385df1cee8b
Matthew B. White <whitemat@uk.ibm.com>
2019-11-04 16:54:44 +0000

    Push the snapshot docker images to nexus
    Change-Id: Ifa1d1297f0c0051d9ec7ff788611a1ce44b9e098
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>

---
+++ ci/publish_jar_nexus.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Exit on first error, print all commands.
+set -e
+set -o pipefail
+WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+function abort {
+    echo "!! Exiting shell script"
+    echo "!!" "$1"
+    exit -1
+}
+
+for binary in shim protos; do
+echo "Pushing fabric-chaincode-$binary.$PUSH_VERSION.jar to maven releases.."
+cp $WORKSPACE/fabric-chaincode-$binary/build/libs/fabric-chaincode-$binary-$PUSH_VERSION.jar $WORKSPACE/fabric-chaincode-$binary/build/libs/fabric-chaincode-$binary.$PUSH_VERSION.jar
+mwn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
+    -DupdateReleaseInfo=true \
+    -Dfile=$WORKSPACE/fabric-chaincode-$binary/build/libs/fabric-chaincode-$binary.$PUSH_VERSION.jar \
+    -DpomFile=$WORKSPACE/fabric-chaincode-$binary/build/publications/"$binary"Jar/pom-default.xml \
+    -DrepositoryId=hyperledger-releases \
+    -Durl=https://nexus.hyperledger.org/content/repositories/releases/ \
+    -DgroupId=org.hyperledger.fabric-chaincode-java \
+    -Dversion=$PUSH_VERSION \
+    -DartifactId=fabric-chaincode-$binary \
+    -DgeneratePom=false \
+    -DuniqueVersion=false \
+    -Dpackaging=jar \
+    -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+done
