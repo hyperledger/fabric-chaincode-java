dc406fcadce95ca31bc329eb401beed1d25d0743
Yury Andreev <andreevym@gmail.com>
2019-12-30 19:16:32 +0300

    [FABCJ-214] - Java chaincode gRPC server for run external chaincode add NettyGrpcServer with method /Connect for start chat with peer without TLS add example external chaincode
    Signed-off-by: Yury Andreev <andreevym@gmail.com>

--- fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/shim/NettyGrpcServerTest.java
+++ fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/shim/NettyGrpcServerTest.java
@@ -33,22 +33,4 @@ class NettyGrpcServerTest {
             e.printStackTrace();
         }
     }
-
-    @Test
-    void stop() {
-        environmentVariables.set("CORE_CHAINCODE_ID_NAME", "mycc");
-        environmentVariables.set("CORE_PEER_ADDRESS", "localhost:7052");
-        environmentVariables.set("CORE_PEER_TLS_ENABLED", "false");
-        environmentVariables.set("CORE_PEER_TLS_ROOTCERT_FILE", "src/test/resources/ca.crt");
-        environmentVariables.set("CORE_TLS_CLIENT_KEY_PATH", "src/test/resources/client.key.enc");
-        environmentVariables.set("CORE_TLS_CLIENT_CERT_PATH", "src/test/resources/client.crt.enc");
-
-        final ChaincodeBase cb = new EmptyChaincode();
-        try {
-            NettyGrpcServer nettyGrpcServer = new NettyGrpcServer(cb);
-            nettyGrpcServer.stop();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
