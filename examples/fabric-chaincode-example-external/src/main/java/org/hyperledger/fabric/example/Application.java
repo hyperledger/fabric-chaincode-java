7242a08a482e8e113ad6c3e6c2125f3926f53ce9
Yury Andreev <andreevym@gmail.com>
2020-01-08 11:07:11 +0300

    [FABCJ-214] - Java chaincode gRPC server for run external chaincode add NettyGrpcServer with method /Connect for start chat with peer without TLS add example external chaincode
    Signed-off-by: Yury Andreev <andreevym@gmail.com>

--- examples/fabric-chaincode-example-external/src/main/java/org/hyperledger/fabric/example/Application.java
+++ examples/fabric-chaincode-example-external/src/main/java/org/hyperledger/fabric/example/Application.java
@@ -16,16 +16,14 @@ import java.io.IOException;
 public class Application {
 
     private static final String PORT_CHAINCODE_SERVER = "PORT_CHAINCODE_SERVER";
+    private static final String CORE_PEER_ADDRESS = "CORE_PEER_ADDRESS";
+    private static final String CORE_CHAINCODE_ID_NAME = "CORE_CHAINCODE_ID_NAME";
 
     /**
      * run application with ENV variable.
      *
      * CORE_CHAINCODE_ID_NAME=externalcc:06d1d324e858751d6eb4211885e9fd9ff74b62cb4ffda2242277fac95d467033;
      * CORE_PEER_ADDRESS=127.0.0.1:7051;
-     * CORE_PEER_TLS_ENABLED=false;
-     * CORE_PEER_TLS_ROOTCERT_FILE=src/test/resources/ca.crt;
-     * CORE_TLS_CLIENT_KEY_PATH=src/test/resources/client.key.enc;
-     * CORE_TLS_CLIENT_CERT_PATH=src/test/resources/client.crt.enc;
      * PORT_CHAINCODE_SERVER=9999;
      *
      *  To enable metrics ensure that there is a standard format Java properites file
@@ -43,9 +41,19 @@ public class Application {
         }
         final int port = Integer.parseInt(portChaincodeServer);
 
+        final String corePeerAddress = System.getenv(CORE_PEER_ADDRESS);
+        if (corePeerAddress == null) {
+            throw new IOException("core peer address not defined in system env. for example 'CORE_PEER_ADDRESS=127.0.0.1:7052'");
+        }
+
+        final String coreChaincodeIdName = System.getenv(CORE_CHAINCODE_ID_NAME);
+        if (coreChaincodeIdName == null) {
+            throw new IOException("core peer address not defined in system env. for example 'CORE_CHAINCODE_ID_NAME=externalcc:06d1d324e858751d6eb4211885e9fd9ff74b62cb4ffda2242277fac95d467033'");
+        }
+
         grpcServerSetting.setPortChaincodeServer(port);
 
-        ChaincodeServer chaincodeServer = new ChaincodeServerImpl(new ContractRouter(new String[] {"-a", "127.0.0.1:7052", "-i", "testId"}), grpcServerSetting);
+        ChaincodeServer chaincodeServer = new ChaincodeServerImpl(new ContractRouter(new String[] {"-a", corePeerAddress, "-i", coreChaincodeIdName}), grpcServerSetting);
         chaincodeServer.start();
     }
 }
