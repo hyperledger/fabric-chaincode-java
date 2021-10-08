f9d02de8d9f0ee1221b3d9637363d4d3b3922e3e
Antoine Toulme <antoine@lunar-ocean.com>
2021-10-07 17:03:39 -0700

    Make test less flaky by removing grpc exporter
    Signed-off-by: Antoine Toulme <antoine@lunar-ocean.com>

--- fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/traces/impl/OpenTelemetryTracesProviderTest.java
+++ fabric-chaincode-shim/src/test/java/org/hyperledger/fabric/traces/impl/OpenTelemetryTracesProviderTest.java
@@ -5,7 +5,6 @@
  */
 package org.hyperledger.fabric.traces.impl;
 
-import com.google.common.io.Closer;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerCall;
@@ -15,13 +14,9 @@ import io.grpc.ServerServiceDefinition;
 import io.grpc.Status;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
-import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import io.grpc.stub.StreamObserver;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse;
-import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
-import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
-import io.opentelemetry.proto.trace.v1.ResourceSpans;
+import io.opentelemetry.sdk.trace.data.SpanData;
 import org.hyperledger.fabric.contract.ChaincodeStubNaiveImpl;
 import org.hyperledger.fabric.metrics.Metrics;
 import org.hyperledger.fabric.protos.peer.Chaincode;
@@ -34,11 +29,8 @@ import org.hyperledger.fabric.shim.ResponseUtils;
 import org.hyperledger.fabric.shim.impl.ChaincodeSupportClient;
 import org.hyperledger.fabric.shim.impl.InvocationTaskManager;
 import org.hyperledger.fabric.traces.Traces;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
@@ -47,36 +39,6 @@ import java.util.concurrent.TimeUnit;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class OpenTelemetryTracesProviderTest {
-
-    private static final class FakeCollector extends TraceServiceGrpc.TraceServiceImplBase {
-        private final List<ResourceSpans> receivedSpans = new ArrayList<ResourceSpans>();
-        private Status returnedStatus = Status.OK;
-
-        @Override
-        public void export(
-                final ExportTraceServiceRequest request,
-                final StreamObserver<ExportTraceServiceResponse> responseObserver) {
-            receivedSpans.addAll(request.getResourceSpansList());
-            responseObserver.onNext(ExportTraceServiceResponse.newBuilder().build());
-            if (!returnedStatus.isOk()) {
-                if (returnedStatus.getCode() == Status.Code.DEADLINE_EXCEEDED) {
-                    // Do not call onCompleted to simulate a deadline exceeded.
-                    return;
-                }
-                responseObserver.onError(returnedStatus.asRuntimeException());
-                return;
-            }
-            responseObserver.onCompleted();
-        }
-
-        List<io.opentelemetry.proto.trace.v1.ResourceSpans> getReceivedSpans() {
-            return receivedSpans;
-        }
-
-        void setReturnedStatus(final Status returnedStatus) {
-            this.returnedStatus = returnedStatus;
-        }
-    }
 
     private class ContextGetterChaincode extends ChaincodeBase {
 
@@ -96,23 +58,6 @@ public final class OpenTelemetryTracesProviderTest {
         }
     }
 
-    private final FakeCollector fakeTracesCollector = new FakeCollector();
-    private final Closer closer = Closer.create();
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        Server server =
-                NettyServerBuilder.forPort(4317)
-                        .addService(fakeTracesCollector)
-                        .build()
-                        .start();
-        closer.register(server::shutdownNow);
-    }
-
-    @AfterEach
-    public void tearDown() throws Exception {
-        closer.close();
-    }
 
     @Test
     public void testProvider() {
@@ -126,13 +71,13 @@ public final class OpenTelemetryTracesProviderTest {
 
     @Test
     public void testTracing() throws Exception {
+
         Properties props = new Properties();
         props.put("CHAINCODE_TRACES_ENABLED", "true");
         props.put("CHAINCODE_TRACES_PROVIDER", OpenTelemetryTracesProvider.class.getName());
         props.put("OTEL_TRACES_SAMPLER", "always_on");
         props.put("OTEL_BSP_SCHEDULE_DELAY", "100");
-        props.put("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317");
-        props.put("OTEL_EXPORTER_OTLP_INSECURE", "true");
+        props.put("otel.traces.exporter", "TestSpanExporterProvider");
         Traces.initialize(props);
         Metrics.initialize(props);
 
@@ -189,8 +134,7 @@ public final class OpenTelemetryTracesProviderTest {
 
         chaincodeSupportClient.start(itm, requestObserver);
         wait.get(5, TimeUnit.SECONDS);
-        Thread.sleep(5000); // wait for async send of the trace
-        List<io.opentelemetry.proto.trace.v1.ResourceSpans> spans = fakeTracesCollector.getReceivedSpans();
+        List<SpanData> spans = TestSpanExporterProvider.SPANS;
         assertThat(spans.isEmpty()).isFalse();
 
         chaincodeSupportClient.shutdown(itm);
